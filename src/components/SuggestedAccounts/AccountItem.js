19a3a3492c108812d51139704ff7899baaf980b7
laugh <laugh@DESKTOP-0LTACEC>
2022-09-16 09:33:02 +0700

    sidebar 1

--- src/components/SuggestedAccounts/AccountItem.js
+++ src/components/SuggestedAccounts/AccountItem.js
@@ -1,27 +1,44 @@
-import PropTypes from 'prop-types';
 import classNames from 'classnames/bind';
+import Tippy from '@tippyjs/react/headless';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCheckCircle } from '@fortawesome/free-solid-svg-icons';
 
+import { Wrapper as PopperWrapper } from '../Popper';
 import styles from './SuggestedAccounts.module.scss';
+import AccountPreview from './AccountPreview/AccountPreview';
 
 const cx = classNames.bind(styles);
 
 function AccountItem() {
-    return (
-        <div className={cx('account-item')}>
-            <img
-                className={cx('avatar')}
-                src="https://p16-sign-va.tiktokcdn.com/tos-useast2a-avt-0068-giso/fac92301a36c2275c99f393061ef04ca~c5_100x100.jpeg?x-expires=1661072400&x-signature=O4VWG2omtPwLwIBpvA%2B5M1EnyS0%3D"
-                alt=""
-            />
-            <div className={cx('item-info')}>
-                <p className={cx('nickname')}>
-                    <strong>nguyenlonghai</strong>
-                    <FontAwesomeIcon className={cx('check')} icon={faCheckCircle}/>
-                </p>
-                <p className={cx('name')}>Nguyễn Long Hải</p>
+
+    const renderPreview = (props) => {
+        return (
+            <div tabIndex="-1" {...props}>
+                <PopperWrapper>
+                    <AccountPreview/>
+                </PopperWrapper>
             </div>
+        )
+    } 
+
+    return (
+        <div>
+            <Tippy interactive delay={[800, 0]} placement="bottom" render={renderPreview}>
+                <div className={cx('account-item')}>
+                    <img
+                        className={cx('avatar')}
+                        src="https://p16-sign-va.tiktokcdn.com/tos-useast2a-avt-0068-giso/fac92301a36c2275c99f393061ef04ca~c5_100x100.jpeg?x-expires=1663462800&x-signature=WP4qEs5DiiizMT3NX0HtMjkHpdk%3D"
+                        alt=""
+                    />
+                    <div className={cx('item-info')}>
+                        <p className={cx('nickname')}>
+                            <strong>nguyenlonghai</strong>
+                            <FontAwesomeIcon className={cx('check')} icon={faCheckCircle} />
+                        </p>
+                        <p className={cx('name')}>Nguyễn Long Hải</p>
+                    </div>
+                </div>
+            </Tippy>
         </div>
     );
 }
